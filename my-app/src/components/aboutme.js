a9ba0dadb0a7c06a66f64fed9beb86aa9c850b4b
Sandy Smith <toosandra@gmail.com>
2020-12-08 08:55:07 -0500

    added more css and worked on about me page.

--- my-app/src/components/aboutme.js
+++ my-app/src/components/aboutme.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import { Grid, Cell, Col, List, ListItem } from 'react-mdl';
+import Headshot from '../assets/Headshot.png';
+//import Carousel from 'react-mdl';
 
 class About extends Component {
     render() {
-        return(
-            <div><h1>About Page</h1></div>
+        return (
+            <div className="about-body">
+                <Grid className="about-grid">
+                    <Cell col={4} className="profile">
+                        <img src={Headshot} alt="headshot" />
+                    </Cell>
+                    <Cell col={8}>
+                        <div className="about-header">
+                            <h3>Hi! I am Sandy Smith!</h3>
+                        </div>
+                        <div>
+                            <List>
+                                <ListItem className="about-data">
+                                    <ul>
+                                        <li>Web Developer</li>
+                                        <li>Fine Art Photographer</li>
+                                        <li>Gardener</li>
+                                        <li>East Tennessean</li>
+                                    </ul>
+                                </ListItem>
+
+                            </List>
+
+
+                        </div>
+                        <div>
+                            <p className="about-content">
+                                While finishing my Bachelor's of Science Degree in Business Management with Western Governor's University, I had realized in my
+                                final semester that aI wanted to get into coding.  I had always wanted to get into coding, but
+                                was always pulled into other directions.  I finally followed my dream, and recently completed
+                                Vanderbilt University's Full Stack Web Developer Bootcamp. WGU required nothing less than a B in each subject, I finished with
+                                a B+, and A with Vanderbilt University.<br />
+                                <br />
+                                My coding skillset includes: HTML5/CSS3, Javascript, jQuery, Bootstrap, Firebase, Node.js, MySQL, Express,
+                                Handlebars, Github, APIs, REST, AJAX, Media Queries, JSON, React.js, and terminal commands.
+                                Other skillsets includes: Corel, LightRoom, Adobe Photoshop, various film and digital
+                                cameras, mareketing, sales, payroll, Human Resources, Oracle, PeopleSoft and Microsoft Office Suite. <br />
+                                <br />
+                                I have experience with working in teams, collaborating and contributing with various levels of team members within a company, and always have
+                                the 'can do attitude'. With my past work history, I can work independently, or work in a team environment either face to face or remote. <br />
+                                <br />
+                                While I love to venture away from home when vacationing, I do enjoy being at home during my downtime.  I can be found doing any of the following: Photographing and
+                                print enhancing, Cooking and entertaining, Writing, Gardening, and playing with my muse - my beloved dogs. <br />
+
+                            </p>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
--- my-app/src/components/landingpage.js
+++ my-app/src/components/landingpage.js
@@ -1,53 +1,86 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
-import Skills from './skills';
+import { Grid, Cell, List, ListItem } from 'react-mdl';
+//import Skills from './skills'
 
 class Landing extends Component {
-    render() {
-        return(
-            <div style={{width: '100', margin: 'auto'}}>
-              <Grid className="landing-grid">
-                <Cell col={12}>
-                  <img 
-                  src="https://th.bing.com/th/id/OIP.rH3MBj2O3hG3ytnIlo2LlgHaHa?w=207&h=207&c=7&o=5&pid=1.7g"
-                  alt="avatar"
-                  className="avatar-img"
-                  />
-                  <div className="banner-text">
-                    <h1>Full Stack Web Developer</h1>
+  render() {
+    return (
+      <div style={{ width: '100', margin: 'auto' }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://th.bing.com/th/id/OIP.rH3MBj2O3hG3ytnIlo2LlgHaHa?w=207&h=207&c=7&o=5&pid=1.7g"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h2>Full Stack Web Developer</h2>
 
-                    <hr/>
-                    <h2>Skills</h2>
-                    <Skills 
-                    skill="Back-End Technology (Node.js, Express Servers, APIs"
+              <hr />
+              <div className="skill-lists">
+                <h3>Skills</h3>
+                
+
+                <div style={{ width: '80%', margin: 'auto' }}>
+                  <Grid className="demo-grid-ruler">
+                    {/* <Cell col={1}></Cell> */}
+                    <Cell col={2}></Cell>
+                    <Cell col={1}>
+                      <i className="fa fa-briefcase" aria-hidden="true" />
+                      <p>BACK-END TECHNOLOGY</p><br/>
+                      <p>Node.js, Express Servers, APIs</p>
+                    </Cell>
+                    {/* <Cell col={1}></Cell> */}
+                    <Cell col={1}></Cell>
+                    <Cell col={1}>
+                      <i className="fa fa-window-restore" aria-hidden="true" />
+                      <p>UI/UX DESIGN</p><br/>
+                      <p>HTML5/CSS3, Bootstrap, React.js</p>
+                    </Cell>
+                    <Cell col={1}></Cell>
                     
-                    skill="UI/UX Design (HTML5/CSS3, Bootstrap, React.js"
-                    skill="Databases (MySql, MongoDB, Firebase)"
-                    skill="Problem Solving (I love to solve puzzles)"
-                    skill="Team Oriented (Experienced team player, ready to contribute and collaborate)"
-                    
-                    />
-                    <div className="social-links">
+                    <Cell col={1}>
+                      <i className="fa fa-database" aria-hidden="true" />
+                      <p>DATABASE</p><br/>
+                      <br/>
+                      <p>MySql, MongoDB, Firebase</p>
+                    </Cell>
+                    <Cell col={1}></Cell>
+                    <Cell col={1}>
+                      <i className="fa fa-puzzle-piece" aria-hidden="true" />
+                      <p>PROBLEM SOLVING & TEAM ORIENTED</p>
+                      <p>I love to solve a puzzle & experienced team player</p>
+                    </Cell>
+           
+                  </Grid>
+                </div>
+              </div>
 
-                      {/* LinkedIn */}
-                      <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-linkedin-square" aria-hidden="true" />
-                      </a>
+              <div className="social-links">
 
-                      {/* Github */}
-                      <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                        <i className="fa fa-github-square" aria-hidden="true" />
-                      </a>
+                {/* LinkedIn */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/* Github */}
+                <a href="http://github.com/slsmi285" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+                  {/* Email */}
+                  <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-envelope" aria-hidden="true" />
+                </a>
 
 
-                    </div>
-                  </div>
-                </Cell>
-              </Grid>
-
+              </div>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+
+      </div>
+    )
+  }
 }
 
 export default Landing;
