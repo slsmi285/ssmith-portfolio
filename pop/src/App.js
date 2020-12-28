b99790dbd0bce11d1810ce63830c9ceab1ff5940
Sandy Smith <toosandra@gmail.com>
2020-12-28 03:01:01 -0500

    worked on layout of react app.

--- pop/src/App.js
+++ pop/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
+import PlaceToVisit from './components/PlaceToVisit';
+// import Footer from './components/Footer';
 
-function App() {
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/dahlia3.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className={classes.root}>
+    <CssBaseline />
+    <Header />
+    <PlaceToVisit />
+    {/* <Footer /> */}
+  </div>
   );
-}
 
-export default App;
+}
---
+++ pop/src/static/places.js
@@ -0,0 +1,28 @@
+const places = [
+    {
+        title: 'About Me',
+        description:
+        "Pink Dhalhia's come in all shapes and sizes.",
+        imageUrl: process.env.PUBLIC_URL + '/assets/dahlia1.jpg',
+        // direction: 'left',
+        time: 1500,
+    },
+    {
+        title: 'Projects',
+        description:
+        "Dhalhia's come in all shapes and sizes.",
+        imageUrl: process.env.PUBLIC_URL + '/assets/dahlia2.jpg',
+        // direction: 'right',
+        time: 1500,  
+    },
+    {
+        title: 'Contact Me',
+        description:
+        "Dhalhia's come in all shapes and sizes.",
+        imageUrl: process.env.PUBLIC_URL + '/assets/dahlia1.jpg',
+        // direction: 'right',
+        time: 1500,  
+    }
+
+];
+export default places;
